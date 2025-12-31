e7117a577e35fea690c25da9c727e26840b407d5
GItconss <forgithub40@gmail.com>
2025-12-31 23:58:39 +0530

    added new folder and files

---
+++ module1/decision_prblm/question-1.py
@@ -0,0 +1,11 @@
+# Check whether a number is odd or even
+num=int(input("enter the number :"))
+if num%2==0:
+    print ("the number is even",num)
+else:
+    print ("the number is odd",num)
+
+
+#output
+# enter the number :98
+# the number is even 98
---
+++ module1/decision_prblm/question-2.py
@@ -0,0 +1,11 @@
+#Find the absolute value of a number
+num = float(input("Enter a number: "))
+absolute_value = abs(num)
+print("Absolute value is:", absolute_value)
+
+
+
+
+#out put
+# Enter a number: -23
+# Absolute value is: 23
